fbd42af48777c3c5bbb79e840bf1143b4edc61d3
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2025-11-06 20:58:32 +0800

    chore: update path alias

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,17 @@
 import { defineNitroConfig } from 'nitropack/config';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineNitroConfig({
   compatibilityDate: 'latest',
   srcDir: 'server',
   imports: false,
+  alias: {
+    server: resolve(__dirname, './server'),
+  },
   runtimeConfig: {
     db: {
       host: '',
